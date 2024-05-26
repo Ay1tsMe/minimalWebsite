8b55b2c7ee4753e5b8af80b900dc26a1d2d6f533
Adam <adam.wyatt@iinet.net.au>
2024-05-26 19:58:30 +0800

    new quotes

--- tuxtower/static/script.js
+++ tuxtower/static/script.js
@@ -1,18 +1,25 @@
 // Random Quotes on home page
 // Array of quotes
 var quotes = [
-    "The only thing we have to fear is fear itself... and Mondays.",
-    "I have a dream that one day all printers will have infinite ink.",
-    "In the end, it's not the years in your life that count, it's the number of unread emails in your inbox.",
-    "Ask not what your Wi-Fi can do for you, ask what you can do for your Wi-Fi.",
-    "I came, I saw, I followed back.",
-    "It is not in the stars to hold our destiny, but in our browser history.",
-    "To be or not to be? That is the system requirements question.",
-    "Float like a butterfly, sting like a bee, code like a developer who forgot to use version control.",
-    "I think, therefore I am... probably overdue for a software update.",
-    "The best way to predict the future is to create it... or find someone who can hack it for you.",
-    "I chat, therefore I GPT.",
-    "The journey of a thousand lines of code begins with a single prompt to ChatGPT.",
+    "Hello World!",
+    "sudo systemctl stop tuxtower",
+    "sudo systemctl start tuxtower",
+    "sudo systemctl restart tuxtower",
+    "iPods are cool, iPhones are cringe",
+    "Hyprland is based",
+    "Wayland is the future",
+    "Sign my guestbook below... Please"
+    "If your keyboard isn't split... what are you doing?",
+    "Church of Emacs",
+    "https://tuxtower.net",
+    "I use arch btw",
+    "Everything I want to do is illegal",
+    "I debloated my workstation",
+    "I am way too dumb to use ubuntu",
+    "I got rid of my mattress, it's bloat",
+    "Look's like Arch Linux broke once again",
+    "Imagine using VS Code",
+    "/home/tuxtower/.config"
   ];
   
   // Function to get a random quote
@@ -26,4 +33,3 @@ var quotes = [
     var quoteElement = document.getElementById("quote");
     quoteElement.textContent = '"' + getRandomQuote() + '"';
   });
-  
